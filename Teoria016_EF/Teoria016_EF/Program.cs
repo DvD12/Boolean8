e3b1c89a46e5c77712dbe4b450a35cae5a0a9770
DvD <dvd1202@gmail.com>
2024-05-08 14:00:58 +0200

    - Teoria 016

--- Teoria016_EF/Teoria016_EF/Program.cs
+++ Teoria016_EF/Teoria016_EF/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Teoria016_EF
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Teoria016_EF
 {
     internal class Program
     {
@@ -12,7 +14,65 @@
                 Email = "a@b.com",
             };
             db.Add(student);
+            //db.Students.Where(x => x.Name.EndsWith("Francesco")).ToList(); // Name LIKE '%Francesco'
             db.SaveChanges();
+
+            Student loadedStudent = db.Students.Where(m => m.StudentId == 1)
+                .Include(s => s.Reviews)
+                .Include(s => s.FrequentedCourses)
+                .FirstOrDefault();
+
+            int numeroRecensioni = loadedStudent.Reviews.Count();
+
+            List<int> numeri = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> numeriDispari = FiltraDispari(numeri);
+            List<int> numeriPari = FiltraPari(numeri);
+
+            // Funzione anonima
+            Func<int, bool> isPari = (x) =>
+            {
+                return x % 2 == 0;
+            };
+            Func<int, int, bool, int, int> ritorna5 = (a, b, c, d) =>
+            {
+                Console.WriteLine(a + b + d);
+                return 5;
+            };
+            var list = numeri.Where(ilMioNumeroGigantesco => ilMioNumeroGigantesco % 2 == 0)
+                .Where(x => x > 5);
+            List<int> numeriPari2 = Filtra(numeri, isPari);
+            List<int> numeriPari3 = Filtra(numeri, x => x % 2 == 0);
+        }
+
+        public static List<int> Filtra(List<int> numeri, Func<int, bool> criterio)
+        {
+            var list = new List<int>();
+
+            foreach (var numero in numeri)
+                if (criterio(numero) == true)
+                    list.Add(numero);
+
+            return list;
+        }
+        public static List<int> FiltraDispari(List<int> numeri)
+        {
+            var list = new List<int>();
+
+            foreach (var numero in numeri)
+                if (numero % 2 == 1)
+                    list.Add(numero);
+
+            return list;
+        }
+        public static List<int> FiltraPari(List<int> numeri)
+        {
+            var list = new List<int>();
+
+            foreach (var numero in numeri)
+                if (numero % 2 == 0)
+                    list.Add(numero);
+
+            return list;
         }
 
         public static int? Divisione(int x, int y)
