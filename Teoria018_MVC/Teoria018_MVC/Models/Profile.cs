11e23162cc00d6591b3d1ec6406c2212eb3988e2
DvD <dvd1202@gmail.com>
2024-05-10 13:00:32 +0200

    - Teoria 018

---
+++ Teoria018_MVC/Teoria018_MVC/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿namespace Teoria018_MVC.Models
+{
+
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+        public Profile() { }
+
+        public Profile(string name, string lastname, int age)
+        {
+            this.Name = name;
+            this.Lastname = lastname;
+            this.Age = age;
+        }
+
+    }
+}
